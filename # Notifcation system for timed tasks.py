10a7df3ee7ea264285a5214dc97a59705897fed3
Oakboater <kristapsimportant@gmail.com>
2025-05-29 13:21:44 +0100

    Files added

---
+++ # Notifcation system for timed tasks.py
@@ -0,0 +1,17 @@
+# Notifcastion system via discord webhooks
+# Using time to have planned activitys
+# Make a file system so the program can remeber the time for certain tasks
+# Make it so we can easily change times, dates and much more using csv files
+import requests
+import time
+import csv
+def List():
+    List = input("Hello, What task would you like to be reminded about?")
+
+# Ask user for List of reminders and time sets, we will be using 24 hour clock, using try so if we have an error it will be fine
+# Save the users list then time in a csv file
+# Make them connect their webhook and save it for easy access
+# Using the time function make the user get a notification via webhook at that time for their task
+# if the user wants to add or remove data make it possible
+
+
